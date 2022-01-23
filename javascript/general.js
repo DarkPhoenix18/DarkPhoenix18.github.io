e6d87cba9fb1aca7ef7b1653bb81cc59cd7751a0
Gridnart <sahuprajval@gmail.com>
2022-01-23 13:35:05 +0530

    Just saving stash

--- javascript/general.js
+++ javascript/general.js
@@ -1,13 +1,1 @@
-let toggleButton=document.getElementsByClassName("toggle-button");
-let navlinks=document.getElementsByClassName("nav-links");
 
-
-function addflex(){
-    if(document.getElementsByClassName('open') == null)
-    toggleButton += 'open';
-
-    else
-    toggleButton -= 'open';
-}
-
-toggleButton.addEventListner('click',addflex());
